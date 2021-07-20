0b4de5da889ed4642bd213d1ffd80e81d367ccb4
Koumori97 <d.e.fledermaus86@gmail.com>
2021-07-20 16:29:33 +0200

    added

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def get_chuck_norris_jokes():
     return "<strong>Chuck Norris Joke: </strong>" + response['value']
 
 
+@app.route('/ting', methods=["GET"])
+def get_chuck_norris_response():
+    api_url = "https://api.chucknorris.io/jokes/random"
+    response = requests.get(api_url).json()
+
+    return "<strong>Chuck Norris Joke: </strong><br>{}".format(response)
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
